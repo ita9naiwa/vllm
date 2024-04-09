5c845242fbf51a97294c1fb1a608a8aa26a79480
Hyunsung Lee <ita9naiwa@gmail.com>
2024-04-09 11:25:28 +0900

    Update LogProbs class in protocol.py

--- vllm/entrypoints/openai/protocol.py
+++ vllm/entrypoints/openai/protocol.py
@@ -331,7 +331,7 @@ class CompletionRequest(BaseModel):
 class LogProbs(BaseModel):
     text_offset: List[int] = Field(default_factory=list)
     token_logprobs: List[Optional[float]] = Field(default_factory=list)
-    tokens: List[str] = Field(default_factory=list)
+    tokens: List[int] = Field(default_factory=list)
     top_logprobs: Optional[List[Optional[Dict[str, float]]]] = None
 
 
